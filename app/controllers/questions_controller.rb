669bc2593188c6a21b6e17059f46d0734a1e28a4
Paulina Kisielewska <paulina.kisielewska6@gmail.com>
2024-02-19 14:29:52 +0100

    Stupid coach ruby on rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,9 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @answers = ["Great!", "Silly question, get dressed and go to work!","I don't care, get dressed and go to work!"]
+  end
+end
